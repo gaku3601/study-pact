e3e9c442f471b8b60791df2a69775e23c5622f2a
gaku0601 <pro.gaku@gmail.com>
2017-12-05 06:57:05 +0900

    README修正

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # study-pact
 CDC testing 勉強用リポジトリ。
+
+## pact-goのインストール
+https://github.com/pact-foundation/pact-go/releases  
+より実行ファイルを取得する。  
+（今回はmacでの作業であるため、0.0.11のdarwinを取得）  
+  
+取得したら、PATHが通っているフォルダへ解凍したフォルダ内のファイルを全てを配置する。  
+(今回は/usr/local/bin)
+これでpact-goコマンドを実行できるようになる。  
+
+## 立ち上げ
+pact-go daemonでサーバを立ち上げる
---
+++ main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+)
+
+func TestMain(t *testing.T) {
+	// Create Pact connecting to local Daemon
+	pact := &dsl.Pact{
+		Port:     6666, // Ensure this port matches the daemon port!
+		Consumer: "MyConsumer",
+		Provider: "MyProvider",
+		Host:     "localhost",
+	}
+	defer pact.Teardown()
+
+	// Pass in test case
+	var test = func() error {
+		u := fmt.Sprintf("http://localhost:%d/foobar", pact.Server.Port)
+		req, err := http.NewRequest("GET", u, strings.NewReader(`{"s":"foo"}`))
+
+		// NOTE: by default, request bodies are expected to be sent with a Content-Type
+		// of application/json. If you don't explicitly set the content-type, you
+		// will get a mismatch during Verification.
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			return err
+		}
+		if _, err = http.DefaultClient.Do(req); err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	// Set up our expected interactions.
+	pact.
+		AddInteraction().
+		Given("User foo exists").
+		UponReceiving("A request to get foo").
+		WithRequest(dsl.Request{
+			Method:  "GET",
+			Path:    "/foobar",
+			Headers: map[string]string{"Content-Type": "application/json"},
+			Body:    `{"s":"foo"}`,
+		}).
+		WillRespondWith(dsl.Response{
+			Status:  200,
+			Headers: map[string]string{"Content-Type": "application/json"},
+			Body:    `{"lastName":"bar"}`,
+		})
+
+	// Verify
+	if err := pact.Verify(test); err != nil {
+		log.Fatalf("Error on Verify: %v", err)
+	}
+}
